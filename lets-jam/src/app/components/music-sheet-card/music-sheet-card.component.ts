beccbf3ce91b40b2e1a3a4ad38c6baa91c6c41b6
Cico96 <40207930+Cico96@users.noreply.github.com>
2022-02-13 17:43:01 +0100

    fix

--- lets-jam/src/app/components/music-sheet-card/music-sheet-card.component.ts
+++ lets-jam/src/app/components/music-sheet-card/music-sheet-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MusicSheet } from 'src/app/model/music-sheet';
+import { MusicsheetService } from 'src/app/services/musicsheet.service';
 
 @Component({
   selector: 'app-music-sheet-card',
@@ -7,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MusicSheetCardComponent implements OnInit {
 
-  constructor() { }
+  ms!: MusicSheet;
+  id = 1;
+
+  constructor(private msService: MusicsheetService) { }
 
   ngOnInit(): void {
-    
+    this.msService.getMusicSheetById(1).subscribe((res) => {
+      this.ms = res;
+    })
   }
 
 }
