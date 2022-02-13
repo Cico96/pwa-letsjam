1245275fd6bb374acdfe78a0cb9fe0d96c9f6b6e
Antonio <antonioang9@gmail.com>
2022-02-13 18:55:26 +0100

    init home

--- lets-jam/src/app/pages/home/home.component.ts
+++ lets-jam/src/app/pages/home/home.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { MusicSheet } from 'src/app/model/music-sheet';
-import { AuthService } from 'src/app/services/auth.service';
-import { MusicsheetService } from 'src/app/services/musicsheet.service';
+import {Component, OnInit} from '@angular/core';
+import {MusicSheet} from 'src/app/model/music-sheet';
+import {AuthService} from 'src/app/services/auth.service';
+import {MusicsheetService} from 'src/app/services/musicsheet.service';
+import {Observable} from "rxjs";
 
 
 @Component({
@@ -11,20 +11,31 @@ import { MusicsheetService } from 'src/app/services/musicsheet.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  
-  constructor(private ms: MusicsheetService, private auth: AuthService) { }
+
+  orderedSheets?: Array<MusicSheet>;
+
+  constructor(private ms: MusicsheetService, private auth: AuthService) {
+  }
 
   ngOnInit(): void {
+    this.orderMusicSheetsBylikes();
   }
 
   getMusicSheet() {
-   console.log(this.ms.getMusicSheetById(1))
-   
-   this.ms.getMusicSheetById(1).subscribe(
-     response => {
-       console.log(response);
-     }
-   )
+    console.log(this.ms.getMusicSheetById(1))
+
+    this.ms.getMusicSheetById(1).subscribe(
+      response => {
+        console.log(response);
+      }
+    )
+  }
+
+  orderMusicSheetsBylikes(): void {
+    let allMusicsheets: Observable<Array<MusicSheet>> = this.ms.getAllMusicSheets();
+    allMusicsheets.subscribe((sheets) => {
+        this.orderedSheets = sheets.sort((a,b) => {return (a.likes - b.likes < 0) ? -1 : (a.likes - b.likes > 0) ? 1 : 0})
+    });
   }
 
 }
