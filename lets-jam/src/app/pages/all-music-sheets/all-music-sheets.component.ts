8c08773134c48914b8d05359c6b9669d772ac43d
Cico96 <40207930+Cico96@users.noreply.github.com>
2022-02-14 23:31:07 +0100

    all music sheets

--- lets-jam/src/app/pages/all-music-sheets/all-music-sheets.component.ts
+++ lets-jam/src/app/pages/all-music-sheets/all-music-sheets.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-all-music-sheets',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllMusicSheetsComponent implements OnInit {
 
-  constructor() { }
+  sidebarForm!: FormGroup;
+
+  constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
