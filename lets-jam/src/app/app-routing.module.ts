ce9d4555250f70c394908159794b889428bb03b1
Antonio <antonioang9@gmail.com>
2022-02-14 22:27:18 +0100

    added routes on routing module and header

--- lets-jam/src/app/app-routing.module.ts
+++ lets-jam/src/app/app-routing.module.ts
@@ -3,15 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import {MusicSheetComponent} from "./pages/music-sheet/music-sheet.component";
+import {SongComponent} from "./pages/song/song.component";
+import {CreateUploadComponent} from "./pages/create-upload/create-upload.component";
+import {AboutUsComponent} from "./pages/about-us/about-us.component";
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
   },
-  { 
-    path: 'home',  
-    component: HomeComponent 
+  {
+    path: 'home',
+    component: HomeComponent
   },
   {
     path: 'login',
@@ -20,6 +24,22 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: 'musicsheets',
+    component: MusicSheetComponent
+  },
+  {
+    path: 'songs',
+    component: SongComponent
+  },
+  {
+    path: 'create-upload',
+    component: CreateUploadComponent
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent
   }
 ];
 
