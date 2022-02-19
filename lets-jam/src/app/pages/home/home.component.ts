1c772cdd9890865a161cf4043240eaaa4eaf6410
Cico96 <40207930+Cico96@users.noreply.github.com>
2022-02-19 17:55:44 +0100

    gnre

--- lets-jam/src/app/pages/home/home.component.ts
+++ lets-jam/src/app/pages/home/home.component.ts
@@ -3,6 +3,8 @@ import {MusicSheet} from 'src/app/model/music-sheet';
 import {AuthService} from 'src/app/services/auth.service';
 import {MusicsheetService} from 'src/app/services/musicsheet.service';
 import {Observable} from "rxjs";
+import { GenreService } from 'src/app/services/genre.service';
+import { Genre } from 'src/app/model/genre';
 
 
 @Component({
@@ -13,29 +15,37 @@ import {Observable} from "rxjs";
 export class HomeComponent implements OnInit {
 
   orderedSheets?: Array<MusicSheet>;
+  allGenre!: Array<string>;
+  musicSheetsByGenre!: Array<MusicSheet>;
 
-  constructor(private ms: MusicsheetService, private auth: AuthService) {
+  constructor(private ms: MusicsheetService, private auth: AuthService, private genreService: GenreService) {
   }
 
   ngOnInit(): void {
+
     this.orderMusicSheetsBylikes();
+    this.musicSheetPerGenre();
   }
 
-  getMusicSheet() {
-    console.log(this.ms.getMusicSheetById(1))
-
-    this.ms.getMusicSheetById(1).subscribe(
-      response => {
-        console.log(response);
-      }
-    )
-  }
 
   orderMusicSheetsBylikes(): void {
     let allMusicsheets: Observable<Array<MusicSheet>> = this.ms.getAllMusicSheets();
     allMusicsheets.subscribe((sheets) => {
         this.orderedSheets = sheets.sort((a,b) => {return (a.likes - b.likes < 0) ? -1 : (a.likes - b.likes > 0) ? 1 : 0})
     });
+  }
+
+  musicSheetPerGenre() {
+
+    this.genreService.getAllGenres().subscribe((gen) => {
+      gen.forEach((g) => {
+        this.allGenre.push(g.name);
+      });
+    });
+
+    this.ms.getAllMusicSheets(undefined, undefined, undefined, this.allGenre, undefined, undefined, undefined, undefined, undefined, undefined).subscribe((ms) => {
+      this.musicSheetsByGenre = ms;
+    })
   }
 
 }
