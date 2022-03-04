23cf8741267396cc427f3058c6d3dd7fe9e16206
Cico96 <40207930+Cico96@users.noreply.github.com>
2022-03-04 12:20:01 +0100

    rearrange music sheet

--- lets-jam/src/app/pages/rearrange-music-sheet/rearrange-music-sheet.component.ts
+++ lets-jam/src/app/pages/rearrange-music-sheet/rearrange-music-sheet.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { FlatComponent } from 'src/app/components/flat/flat.component';
 import { MusicSheet } from 'src/app/model/music-sheet';
+import { NewMusicSheet } from 'src/app/model/requests-model/new-music-sheet';
+import { NewMusicSheetSong } from 'src/app/model/requests-model/new-music-sheet-song';
 import { MusicsheetService } from 'src/app/services/musicsheet.service';
 
 @Component({
@@ -15,6 +18,11 @@ export class RearrangeMusicSheetComponent implements OnInit {
   score!: string;
   msAuthor!: string;
   msTitle!: string;
+  musicSheet!: MusicSheet;
+  newMusicSheet?: NewMusicSheet;
+  newMusicSheetSong?: NewMusicSheetSong;
+
+  @ViewChild(FlatComponent) child!: FlatComponent;
 
   constructor(private musicSheetService: MusicsheetService, private route: ActivatedRoute, private formBuilder: FormBuilder) { }
 
@@ -24,6 +32,9 @@ export class RearrangeMusicSheetComponent implements OnInit {
       this.musicSheetService.getMusicSheetData(res['id']).subscribe((s) => {
         this.score = s.content;
       });
+      this.musicSheetService.getMusicSheetById(res['id']).subscribe((ms) => {
+        this.musicSheet = ms;
+      })
     });
 
     this.rearrangeForm = this.formBuilder.group({
@@ -35,7 +46,29 @@ export class RearrangeMusicSheetComponent implements OnInit {
   }
 
   rearrangeSheet() {
-    //handle form
+    
+    this.child.getCurrentJsonSheet().then((data: any) => {
+
+      this.newMusicSheetSong = {
+        id: this.musicSheet.song.id,
+        spotifyId: this.musicSheet.song.spotifyId,
+        songtype: Boolean(0),
+        title: this.musicSheet.song.title,
+        author: this.musicSheet.song.author,
+        genreId: this.musicSheet.song.genre.id
+      }
+      this.newMusicSheet = {
+        title: this.rearrangeForm.get('title')?.value,
+        author: this.rearrangeForm.get('author')?.value,
+        visibility: Boolean(this.rearrangeForm.get('visibility')?.value),
+        rearranged: true,
+        content: JSON.stringify(data),
+        song: this.newMusicSheetSong
+      }
+      this.musicSheetService.addMusicSheet(this.newMusicSheet).subscribe(() => {
+        console.log('apposto');
+      })
+    })
   }
 
 }
