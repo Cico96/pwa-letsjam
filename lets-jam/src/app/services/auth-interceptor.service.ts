ca936a0c26b83f05e4b850265310f8608cf51623
Cico96 <40207930+Cico96@users.noreply.github.com>
2022-03-08 00:24:05 +0100

    fx

--- lets-jam/src/app/services/auth-interceptor.service.ts
+++ lets-jam/src/app/services/auth-interceptor.service.ts
@@ -43,7 +43,7 @@ export class AuthInterceptorService implements HttpInterceptor {
 
   private handleAuthError(err: HttpErrorResponse): Observable<any> {
     //handle your auth error or rethrow
-    if (err.status === 401 || err.status === 403) {
+    if (err.status === 401 || err.status === 403) { 
         //navigate /delete cookies or whatever
         this.router.navigate(['login']);
         // if you've caught / handled the error, you don't want to rethrow it unless you also want downstream consumers to have to handle it as well.
